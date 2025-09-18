954c00219f82f8920ee6c082b868afa3a3c66bac
yotchikov <books.antony@gmail.com>
2025-09-18 05:56:27 +0300

    Updates

---
+++ .storybook/preview.ts
@@ -0,0 +1,15 @@
+import type { Preview } from '@storybook/react-vite';
+import 'ui-kit/styles/index.css';
+
+const preview: Preview = {
+  parameters: {
+    controls: {
+      matchers: {
+        color: /(background|color)$/i,
+        date: /Date$/i,
+      },
+    },
+  },
+};
+
+export default preview;
